740e10abfab9e709f6e7f6dc98fdd841eaaaba02
sasali <w.siteee@gmail.com>
2016-05-21 19:27:28 -0700

    Error when exec npm run eslint. (https://github.com/eslint/eslint/issues/5569);

--- src/telegram.js
+++ src/telegram.js
@@ -22,7 +22,6 @@ class TelegramBot extends EventEmitter {
     'location', 'new_chat_participant', 'left_chat_participant', 'new_chat_title',
     'new_chat_photo', 'delete_chat_photo', 'group_chat_created'
   ];
-  
   /**
    * Both request method to obtain messages are implemented. To use standard polling, set `polling: true`
    * on `options`. Notice that [webHook](https://core.telegram.org/bots/api#setwebhook) will need a SSL certificate.
