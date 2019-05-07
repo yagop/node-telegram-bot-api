1c16ea706117646ab4c2b43b378a9a153d46dd6d
ILoveYouDrZaius <iloveyoudoctorzaius@gmail.com>
2019-05-07 18:52:02 +0200

    Fixed index.js in order to make it work in browsers

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
  * Otherwise, we use the ES6 code.
  * We are deprecating support for Node.js v5.x and below.
  */
-const majorVersion = parseInt(process.versions.node.split('.')[0], 10);
-if (majorVersion <= 5) {
-  const deprecate = require('depd')('node-telegram-bot-api');
-  deprecate('Node.js v5.x and below will no longer be supported in the future');
-  module.exports = require('./lib/telegram');
+if ('node' in process.versions) {
+  const majorVersion = parseInt(process.versions.node.split('.')[0], 10);
+  if (majorVersion <= 5) {
+    const deprecate = require('depd')('node-telegram-bot-api');
+    deprecate('Node.js v5.x and below will no longer be supported in the future');
+    module.exports = require('./lib/telegram');
+  } else {
+    module.exports = require('./src/telegram');
+  }
 } else {
   module.exports = require('./src/telegram');
 }
