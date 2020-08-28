23cf929d82304b1fd08c6603e478fc31c2c6689c
Amirshayan <amirshayanaghamiri1@gmail.com>
2020-08-29 03:55:48 +0430

    Make some improvements

--- index.js
+++ index.js
@@ -4,10 +4,13 @@
  * We are deprecating support for Node.js v5.x and below.
  */
 const majorVersion = parseInt(process.versions.node.split('.')[0], 10);
-if (majorVersion <= 5) {
+if (majorVersion <= 5) 
+{
   const deprecate = require('depd')('node-telegram-bot-api');
   deprecate('Node.js v5.x and below will no longer be supported in the future');
   module.exports = require('./lib/telegram');
-} else {
+} 
+else 
+{
   module.exports = require('./src/telegram');
 }
