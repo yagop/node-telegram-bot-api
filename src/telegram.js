a379266b56a8a515fb7d52f31f1d4807f74a8218
daniele <zeolla.d@gmail.com>
2022-08-08 21:07:22 +0200

    Fixed sliceIntoChuks

--- src/telegram.js
+++ src/telegram.js
@@ -85,7 +85,7 @@ function stringify(data) {
  */
 function sliceIntoChunks (arr, chunkSize) {
   const res = [];
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i += chunkSize) {
       const chunk = arr.slice(i, i + chunkSize);
       res.push(chunk);
   }
