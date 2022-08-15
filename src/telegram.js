e495f40a01d7e58a8a9d4e536cedaae475e6bb6c
danielperez9430 <dpefe@hotmail.es>
2022-08-15 21:40:47 +0200

    fix: docs

--- src/telegram.js
+++ src/telegram.js
@@ -2162,7 +2162,7 @@ class TelegramBot extends EventEmitter {
   /**
    * Use this method to get information about custom emoji stickers by their identifiers.
    *
-   * @param  {String|Array} custom_emoji_ids List of custom emoji identifiers. At most 200 custom emoji identifiers can be specified.
+   * @param  {Array} custom_emoji_ids List of custom emoji identifiers. At most 200 custom emoji identifiers can be specified.
    * @param  {Object} [options] Additional Telegram query options
    * @return {Promise} Array of [Sticker](https://core.telegram.org/bots/api#sticker) objects.
    * @see https://core.telegram.org/bots/api#getcustomemojistickers
