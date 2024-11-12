29b8b1453436ce74d90957af5fca99afdd519693
ArhsiZahid <za66826@gmail.com>
2024-11-12 23:27:47 +0530

    committed

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
  * We are deprecating support for Node.js v5.x and below.
  */
 const majorVersion = parseInt(process.versions.node.split('.')[0], 10);
-if (majorVersion <= 5) {
+if (majorVersion <= 5 && majorVersion <=10)  {
   const deprecate = require('./src/utils').deprecate;
   deprecate('Node.js v5.x and below will no longer be supported in the future');
   module.exports = require('./lib/telegram');
 } else {
-  module.exports = require('./src/telegram');
+  module.exports = require('./src/telegram')
 }
